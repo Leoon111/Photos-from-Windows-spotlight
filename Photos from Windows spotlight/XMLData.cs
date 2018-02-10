6bd8bae64d814e4af25bbd7deb33964ba9c61f22
Leonid Petrov <leoon111@gmail.com>
2018-02-10 14:51:34 +0300

    class templates for working with XML are added and bin and obj are removed 1

---
+++ Photos from Windows spotlight/XMLData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Photos_from_Windows_spotlight
+{
+    class XMLData
+    {
+        /// <summary>
+        /// чтение данных из файла XML
+        /// </summary>
+        public void Read()
+        {
+
+        }
+
+        /// <summary>
+        /// запись данных в файл XML
+        /// </summary>
+        public void Write()
+        {
+
+        }
+
+        /// <summary>
+        /// Создание файла с данными, если таковой не имеется.
+        /// </summary>
+        public void CreateNewDateFile()
+        {
+
+        }
+    }
+}
