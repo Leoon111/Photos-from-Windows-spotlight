51eacfa787d775f827ef6f9caa28a1c0fe158d18
Леонид <leooncom@hotmail.com>
2021-02-01 18:41:50 +0300

    ViewModel и Locator для инверсии управления и внедрения зависимости

---
+++ PhotoFromScreensaver/ViewModels/VeiwModelLocator.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PhotoFromScreensaver.ViewModels
+{
+    class VeiwModelLocator
+    {
+        public MyWindowsViewModel MyWindowsViewModel => App.Services.GetRequiredService<MyWindowsViewModel>();
+    }
+}
