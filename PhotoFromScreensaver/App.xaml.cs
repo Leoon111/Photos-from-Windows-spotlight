59f7c9991ff622db64c213dcf92fcba2ca3a0532
Леонид <leooncom@hotmail.com>
2021-02-01 18:41:50 +0300

    ViewModel и Locator для инверсии управления и внедрения зависимости

--- PhotoFromScreensaver/App.xaml.cs
+++ PhotoFromScreensaver/App.xaml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PhotoFromScreensaver.ViewModels;
 
 namespace PhotoFromScreensaver
 {
@@ -13,5 +11,18 @@ namespace PhotoFromScreensaver
     /// </summary>
     public partial class App : Application
     {
+        private static IHost _Hosting;
+
+        public static IHost Hosting => _Hosting
+            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
+                .ConfigureServices(ConfigureServices)
+                .Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MyWindowsViewModel>();
+        }
     }
 }
---
+++ PhotoFromScreensaver/ViewModels/MyWindowsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Photos_Windows_spotlight.ViewModels.Base;
+
+namespace PhotoFromScreensaver.ViewModels
+{
+    class MyWindowsViewModel : ViewModel
+    {
+        private string _Title = "Фото с заставки Windows. версия 0.6";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+    }
+}
